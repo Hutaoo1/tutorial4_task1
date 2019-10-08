dfa2aac0da0ea21e2f8a2dd5f39a25451e9725f6
Hu Tao <hutao19990508@sina.com>
2019-10-08 22:46:08 +0800

    add .gitignore

---
+++ src/main/java/g/Calc.java
@@ -0,0 +1,17 @@
+package g;
+
+
+public class Calc {
+	
+	
+	
+	public  int add (int i, int y) {
+		int sum = i+y;
+		return sum;
+	}
+	
+	public  int subtract (int i, int y) {
+		int sub = i*y;
+		return sub;
+	}
+}
---
+++ src/test/java/g/AppTest.java
@@ -0,0 +1,40 @@
+package g;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+        Calc aCalc =new Calc();
+        assertEquals(3, aCalc.add(1, 2));
+    }
+}
